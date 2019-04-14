6bbc87bf2334f7a51739aa1610abdc4a41605784
Jeffrey Warren <jeff@unterbahn.com>
2019-04-14 17:50:17 -0500

    less redundant code

--- app.rb
+++ app.rb
@@ -26,26 +26,7 @@ get '/export' do
   @data = open(params[:url]).read
   @data = JSON.parse(@data)
 
-  export = Export.new
-
-  @data = @data.keep_if do |w|
-    w['nodes'] && w['nodes'].length > 0 && w['cm_per_pixel'] && w['cm_per_pixel'].to_f > 0
-  end
-
-  scale = params[:scale] || @data[0]['cm_per_pixel']
-  map_id = params[:map_id] || @data[0]['map_id']
-  id = params[:id] || @data[0]['id']
-  key = params[:key] || ''
-
-  MapKnitterExporter.run_export(
-    id, # sources from first image
-    scale,
-    export,
-    map_id,
-    ".",
-    @data,
-    key
-  )
+  run_export(@data)
 end
 
 post '/export' do
@@ -57,17 +38,20 @@ post '/export' do
   end
   STDERR.puts "Uploading file, original name #{name.inspect}"
   @data = JSON.parse(tmpfile.read)
-  String @data[0]['image_file_name']
 
+  run_export(@data)
+end
+
+def run_export
   export = Export.new
 
-  @data = @data.keep_if do |w|
+  data = data.keep_if do |w|
     w['nodes'] && w['nodes'].length > 0 && w['cm_per_pixel'] && w['cm_per_pixel'].to_f > 0
   end
 
-  scale = params[:scale] || @data[0]['cm_per_pixel']
-  map_id = params[:map_id] || @data[0]['map_id']
-  id = params[:id] || @data[0]['id']
+  scale = params[:scale] || data[0]['cm_per_pixel']
+  map_id = params[:map_id] || data[0]['map_id']
+  id = params[:id] || data[0]['id']
   key = params[:key] || ''
 
   MapKnitterExporter.run_export(
@@ -76,7 +60,7 @@ post '/export' do
     export,
     map_id,
     ".",
-    @data,
+    data,
     key
   )
 end
